14a483a4b727420bdd186da6fc2cc566a5492369
Philip Manijak <github@exclsr.com>
2018-04-09 22:42:11 -0700

    Config: Move SERVER_URL to proper files

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
-import config from '@/config.js'
 
 export default axios.create({
-   baseURL: config.server,
+   baseURL: process.env.SERVER_URL,
    withCredentials: true
 });
