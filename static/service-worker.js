527da19c0cb94f9cb9aff31953374ed3419b286f
Philip Manijak <github@exclsr.com>
2018-04-09 21:58:55 -0700

    PWA: Audit fixes
     * Show something when JavaScript disabled
     * Add a basic service worker
     * Fix the path to the logo

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,1 +1,72 @@
-// Do nothing
+// service-worker.js
+
+var TEH_CACHE = 'circle-st-cache-v1';
+// TODO: Populate via webpack?
+var urlsToCache = [
+  '/',
+  '/#/'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(TEH_CACHE)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+        var url = fetchRequest.url;
+
+        var request = {
+            url: fetchRequest.url,
+            headers: fetchRequest.headers,
+            context: fetchRequest.context,
+            referrer: fetchRequest.referrer,
+            referrerPolicy: fetchRequest.referrerPolicy,
+            mode: fetchRequest.mode,
+            redirect: fetchRequest.redirect,
+            integrity: fetchRequest.integrity,
+            cache: fetchRequest.cache,
+            credentials: 'include'
+        };
+
+        return fetch(url, request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(TEH_CACHE)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
